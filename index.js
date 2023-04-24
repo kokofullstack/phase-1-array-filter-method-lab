d117651ba2c9e55f59a77bda3cf18efbabf85c6f
Maung Ko Ko <mwkoko2020@gmail.com>
2023-04-23 19:58:24 -0600

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (names, string) => {
+    return names.filter(names => names.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(names,string){
+    const driversMatch = [];
+    for(const drivers of names){
+        if( string === names)
+        {
+            return driversMatch.push(drivers)
+        }
+    }
+    return  names.filter(names => names[0] === string[0])
+}
+
+function matchName(drivers){
+    return drivers === 'Bobby'
+}
+const output = drivers.filter(matchName);
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) { return driver.name === argument })
+  }
+
+
+
+  
